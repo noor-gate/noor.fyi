ef3c4b9b54a44c49a2bae222c14b6350adcc22cc
Noor <noorsawhney@yahoo.co.uk>
2020-09-23 19:06:36 +0100

    Updates

---
+++ precache-manifest.8a7cd6a1175578843ef309bcbdb0ee9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155c6ab44c14fbecd54f8d42f01f3dad",
+    "url": "/noor.fyi/index.html"
+  },
+  {
+    "revision": "ca728e4be02f217d834e",
+    "url": "/noor.fyi/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "559f0213502dcb0de739",
+    "url": "/noor.fyi/static/css/main.6a6668e7.chunk.css"
+  },
+  {
+    "revision": "ca728e4be02f217d834e",
+    "url": "/noor.fyi/static/js/2.4aa04fee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/noor.fyi/static/js/2.4aa04fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559f0213502dcb0de739",
+    "url": "/noor.fyi/static/js/main.b2efe176.chunk.js"
+  },
+  {
+    "revision": "d3d63dcb5d2fa6f18ebd",
+    "url": "/noor.fyi/static/js/runtime-main.be33586b.js"
+  }
+]);
